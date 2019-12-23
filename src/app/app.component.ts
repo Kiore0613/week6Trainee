92825a5006ddd82caff436e6128f51efc8345dc9
Kiore <ricardochicas95@gmail.com>
2019-12-22 23:34:41 -0600

    ngFor TrackBy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'week6Trainee';
-  courses = [
-    { id: 1, name: 'course1 ' },
-    { id: 2, name: 'course2' },
-    { id: 2, name: 'course3' }
-  ];
-
-  onAdd() {
-    this.courses.push({ id: 4, name: 'course4' });
+  courses: object[];
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'course1 ' },
+      { id: 2, name: 'course2' },
+      { id: 2, name: 'course3' }
+    ];
   }
 
-  onChange(course) {
-    course.name = 'UPDATE';
+  trackCourse(index: number, course) {
+    return course ? course.id : undefined;
   }
 }
